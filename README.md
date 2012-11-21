dd9570e9a8d4afff613be0fd7d2ccf4c465c4802
Dmitry Shirokov <deadrunk@gmail.com>
2012-11-21 16:32:33 +0400

    first commit

--- README.md
+++ README.md
@@ -2,3 +2,24 @@ node-geo
 ========
 
 nodejs geo lookup module
+
+## GEO databases
+
+You can download fresh geo database from this website: http://ipgeobase.ru/.
+Database contains major ranges for all countries worldwide and presice ranges for most Russian cities/regions.
+Also it could be extended with another geo data.
+
+----
+
+
+## Usage
+
+	var geo = require('node-geo');
+	var res = geo.lookup("87.229.134.24");
+
+## Speed / Benchmark
+
+	Approximate speed on average laptop is about 1.5M calls per second. Benchmark is simple, but it gives you an idea about module's performance. Test it yourself:
+
+	node ./lib/bench.js
+
---
+++ test/geo.js
@@ -0,0 +1,29 @@
+
+var assert = require("assert"),
+    geo  = require("../lib/geo");
+
+describe("geo#ip2long", function() {
+    it("IP should be converted to the long format", function() {
+        var result = geo.ip2long("87.229.134.24");
+        assert.equal(result, 1474659864);
+    });
+});
+
+describe("geo#lookup", function() {
+    it("should find country and region", function() {
+        var result = geo.lookup("87.229.134.24");
+        assert.equal(result.country, "RU");
+        assert.equal(result.region, 1056);
+    });
+
+    it("should return null for unknown IP", function() {
+        var result = geo.lookup("1.1.1.1");
+        assert.equal(result, null);
+    });
+
+    it("should work correctly with unknown region", function() {
+        var result = geo.lookup("2.20.4.0");
+        assert.equal(result.country, "IT");
+        assert.equal(result.region, 0);
+    });
+});
